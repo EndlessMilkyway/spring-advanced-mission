9c6ec2903f0547f92f84be2a3dfd242cd4fe9b7d
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-03-26 22:50:57 +0900

    feat: User 엔티티 Validation 어노테이션 변경
    - name 필드의 @NotBlank을 @NotNull로 변경
    - phone 필드의 @NotBlank을 @NotNull로 변경
    - ""과 " "에 대한 예외는 @Pattern 어노테이션에서 처리

--- src/main/java/com/endlessmilkyway/springadvancedmission/entity/User.java
+++ src/main/java/com/endlessmilkyway/springadvancedmission/entity/User.java
@@ -15,19 +15,19 @@ public class User {
     @Size(min = 6, message = "아이디는 6글자 이상 입력해야 합니다!")
     private String id;
 
-    @NotNull
+    @NotNull(message = "비밀번호 값이 제대로 들어오지 않았습니다!")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*()-+=]).{10,20}$", message = "비밀번호 형식이 올바르지 않습니다! 8자리 이상, 대소문자 포함, 숫자 및 특수문자 포함(!@#%^&*()-+=)")
     private String pw;
 
-    @NotBlank(message = "이름은 공백일 수 없습니다!")
+    @NotNull(message = "이름값이 제대로 들어오지 않았습니다!")
     @Pattern(regexp = "^[가-힣]+$", message = "이름은 한글만 입력 가능합니다!")
     private String name;
 
-    @NotBlank(message = "전화번호는 공백일 수 없습니다!")
+    @NotNull(message = "전화번호 값이 제대로 들어오지 않았습니다!")
     @Pattern(regexp = "^01(?:0|1|[6-9])-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다!")
     private String phone;
 
-    @NotBlank(message = "이메일은 공백일 수 없습니다!")
+    @NotBlank(message = "이메일 값이 제대로 들어오지 않았습니다!")
     @Email(message = "이메일 형식이 올바르지 않습니다!")
     private String email;
 }
