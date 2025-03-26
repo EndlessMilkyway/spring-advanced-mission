41374695dce9515994fc26f7808b5c7e1098349c
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-03-26 13:32:09 +0900

    init: initial project

---
+++ src/main/java/com/endlessmilkyway/springadvancedmission/SpringAdvancedMissionApplication.java
@@ -0,0 +1,13 @@
+package com.endlessmilkyway.springadvancedmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endlessmilkyway/springadvancedmission/SpringAdvancedMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endlessmilkyway.springadvancedmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
